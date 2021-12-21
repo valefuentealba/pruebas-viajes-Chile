97052866649897b5fdd8670625d42bc4d5af89f6
valefuentealba <valeriafuentealba@udec.cl>
2021-12-20 21:04:07 -0300

    typeform formulario

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+    $("a").click(function(event) {
+        // Con este if se asegura que this.hash tenga un valor antes de anular el comportamiento predeterminado
+        if (this.hash !== "") {
+            // Previene el comportamiento de click predeterminado
+            event.preventDefault();
+            // Guarda el valor del hash en una variable llamada gato
+            var gato = this.hash;
+            // Usa el método animate para animar el scroll y hacerlo de una forma suave
+            // El número opciona (800) especifica el número de milisegundos que se demorara en llegar hasta el área
+            $('html, body').animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() { // Agrega hash (#) a la URL cuando haya terminado de desplazarse(comportamiento de click predeterminado)
+                window.location.hash = gato;
+            });
+        } // Fin del if
+    });
+});
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
