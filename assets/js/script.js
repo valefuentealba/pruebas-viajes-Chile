e52dcf9314097c2bface40371f1ca110c57b8fe2
valefuentealba <valeriafuentealba@udec.cl>
2021-12-21 23:01:41 -0300

    Scroll Modificación y retoques

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,19 @@
-$(function() {
-    $("a").click(function(event) {
-        // Con este if se asegura que this.hash tenga un valor antes de anular el comportamiento predeterminado
-        if (this.hash !== "") {
-            // Previene el comportamiento de click predeterminado
-            event.preventDefault();
-            // Guarda el valor del hash en una variable llamada gato
-            var gato = this.hash;
-            // Usa el método animate para animar el scroll y hacerlo de una forma suave
-            // El número opciona (800) especifica el número de milisegundos que se demorara en llegar hasta el área
-            $('html, body').animate({
-                scrollTop: $(gato).offset().top
-            }, 800, function() { // Agrega hash (#) a la URL cuando haya terminado de desplazarse(comportamiento de click predeterminado)
-                window.location.hash = gato;
-            });
-        } // Fin del if
-    });
-});
+//Fondo scroll a navbar
+$(window).scroll(function() {
 
+    if ($(this).scrollTop() > 100) {
+        $(".navbar").css({
+            "background": "#17a2b8"
+        })
+    } else {
+        $(".navbar").css({
+            "background": "transparent"
+        })
+    }
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+//Color navbar
+$(".navbar-toggler").click(function() {
+    $(".navbar").toggleClass('bg-info');
+});
