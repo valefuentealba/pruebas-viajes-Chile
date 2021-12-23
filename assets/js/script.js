7d3ee08bebfa1be8986ea8f107496cea21ca0e6e
valefuentealba <valeriafuentealba@udec.cl>
2021-12-23 19:29:39 -0300

    código final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-$(document).ready(function() {
-    $("a").click(function() {
-        var gato = this.hash;
-        $("html, body").animate({
-                scrollTop: $(gato).offset().top - 50
-            },
-            800
-        );
-    });
-    $(nav).scrollTop(function() {
-        var color = this.bash;
-        $(color).body("#ffffff").color("#000000");
-    });
-});
-
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
